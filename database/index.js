60476630cce756c191099a5ad62c44650f5edde0
Mark Hayford <mhayford29@gmail.com>
2019-10-11 14:20:06 -0700

    changed syntax for requiring config file

--- database/index.js
+++ database/index.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-const password = require('../config');
+const PASSWORD = process.env.PASSWORD || require('../config');
 
 //mongoose.connect('mongodb://localhost/cfb_ranker', {useNewUrlParser: true});
 if(process.env.PASSWORD){
   mongoose.connect(`mongodb+srv://mhayford29:${process.env.PASSWORD}@cluster0-fu2nf.mongodb.net/cfb_ranker?retryWrites=true&w=majority`, {useNewUrlParser: true});
 } else{
-  mongoose.connect(`mongodb+srv://mhayford29:${password}@cluster0-fu2nf.mongodb.net/cfb_ranker?retryWrites=true&w=majority`, {useNewUrlParser: true});
+  mongoose.connect(`mongodb+srv://mhayford29:${PASSWORD}@cluster0-fu2nf.mongodb.net/cfb_ranker?retryWrites=true&w=majority`, {useNewUrlParser: true});
 }
 
 var db = mongoose.connection;
