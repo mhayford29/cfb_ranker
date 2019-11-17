5a4bbb1b00643c637021a6b37f65360111a105d1
Mark Hayford <mhayford29@gmail.com>
2019-11-17 14:00:48 -0800

    created a userPolls schema

--- database/models.js
+++ database/models.js
@@ -13,18 +13,41 @@ var gameIdsSchema = new Schema({
   location: String,
   logos: [String],
   stats: [{ name: String, value: Number }]
-}, {strict: 'false'})
+}, { strict: 'false' })
 
 var gameDataSchema = new Schema({
   id: String,
   week: Number, 
   data: {}
-}, {strict: 'false'})
+}, { strict: 'false' })
 
+var userPollsSchema = new Schema({
+  email: String,
+  year: Number,
+  1: {},
+  2: {},
+  3: {},
+  4: {},
+  5: {},
+  6: {},
+  7: {},
+  8: {},
+  9: {},
+  10: {},
+  11: {},
+  12: {},
+  13: {},
+  14: {},
+  15: {},
+  final: {}
+}, { strict: 'false' })
+
+var UserPolls = mongoose.model('UserPolls', userPollsSchema);
 var GameIds = mongoose.model('GameIds', gameIdsSchema);
 var GameData = mongoose.model('GameData', gameDataSchema);
 
 module.exports = {
+  UserPolls,
   GameIds,
   GameData
 };
