abd8e245c96f043ab52d88d3f1750a6d1f0313e7
Mark Hayford <mhayford29@gmail.com>
2019-12-05 14:24:35 -0800

    changed Poll component from class to functional

--- client/dist/bundle.js
+++ client/dist/bundle.js
@@ -36325,80 +36325,36 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var _containers_conferenceItemContainer_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(123);
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
 
 
 
+var Poll = function Poll(props) {
+  var id = props.match.params.id;
 
-var Poll =
-/*#__PURE__*/
-function (_React$Component) {
-  _inherits(Poll, _React$Component);
-
-  function Poll(props) {
-    _classCallCheck(this, Poll);
-
-    return _possibleConstructorReturn(this, _getPrototypeOf(Poll).call(this, props));
-  } // componentDidMount(){
-  //   this.props.fetchPoll(this.props.id);
-  // }
-  // componentDidUpdate(prevProps){
-  //   if(this.props.id !== prevProps.id){
-  //     this.props.fetchPoll(this.props.id)
-  //   }
-  // }
-
-
-  _createClass(Poll, [{
-    key: "render",
-    value: function render() {
-      var props = this.props;
-      var id = props.match.params.id;
-
-      if (props.publishedPolls.length) {
-        return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, props.publishedPolls[id].name), props.publishedPolls[id].ranks.map(function (team, index) {
-          return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_containers_conferenceItemContainer_js__WEBPACK_IMPORTED_MODULE_1__["PollItemContainer"], {
-            team: team,
-            rank: index + 1,
-            addToRankings: props.addToRankings
-          });
-        }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-          style: {
-            marginBottom: '30px'
-          }
-        }, "Others recieving votes: ", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("br", null), props.publishedPolls[id].others.map(function (team, index) {
-          return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {
-            style: {
-              fontSize: '25px'
-            }
-          }, team.team.location, ": ", team.points, ",", '  ');
-        })));
-      } else {
-        return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-          className: "lds-dual-ring"
-        });
+  if (props.publishedPolls.length) {
+    return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, props.publishedPolls[id].name), props.publishedPolls[id].ranks.map(function (team, index) {
+      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_containers_conferenceItemContainer_js__WEBPACK_IMPORTED_MODULE_1__["PollItemContainer"], {
+        team: team,
+        rank: index + 1,
+        addToRankings: props.addToRankings
+      });
+    }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+      style: {
+        marginBottom: '30px'
       }
-    }
-  }]);
-
-  return Poll;
-}(react__WEBPACK_IMPORTED_MODULE_0___default.a.Component);
+    }, "Others recieving votes: ", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("br", null), props.publishedPolls[id].others.map(function (team, index) {
+      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {
+        style: {
+          fontSize: '25px'
+        }
+      }, team.team.location, ": ", team.points, ",", '  ');
+    })));
+  } else {
+    return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+      className: "lds-dual-ring"
+    });
+  }
+};
 
 /* harmony default export */ __webpack_exports__["default"] = (Poll);
 
--- client/src/components/Poll.jsx
+++ client/src/components/Poll.jsx
@@ -1,46 +1,29 @@
 import React from 'react';
 import { PollItemContainer } from '../containers/conferenceItemContainer.js';
 
-class Poll extends React.Component{
-  constructor(props){
-    super(props)
-  }
-
-  // componentDidMount(){
-  //   this.props.fetchPoll(this.props.id);
-  // }
-
-  // componentDidUpdate(prevProps){
-  //   if(this.props.id !== prevProps.id){
-  //     this.props.fetchPoll(this.props.id)
-  //   }
-  // }
-
-  render(){
-    const { props } = this;
-    const { id } = props.match.params;
-    if(props.publishedPolls.length){
-      return(
+var Poll = (props) => {
+  const { id } = props.match.params;
+  if(props.publishedPolls.length){
+    return(
+      <div>
         <div>
-          <div>
-            {props.publishedPolls[id].name}
-          </div>
-          {props.publishedPolls[id].ranks.map((team, index) => {
-            return <PollItemContainer team={team} rank={index + 1} addToRankings={props.addToRankings}/>
-          })}
-          <div style={{ marginBottom: '30px' }}>
-            Others recieving votes: <br/>
-            {props.publishedPolls[id].others.map((team, index) => {
-              return <span style={{ fontSize: '25px' }}>{team.team.location}: {team.points},{'  '}</span>
-            })}
-          </div>
+          {props.publishedPolls[id].name}
         </div>
-      )
-    } else{
-      return(
-        <div className="lds-dual-ring"></div>
-      )
-    }
+        {props.publishedPolls[id].ranks.map((team, index) => {
+          return <PollItemContainer team={team} rank={index + 1} addToRankings={props.addToRankings}/>
+        })}
+        <div style={{ marginBottom: '30px' }}>
+          Others recieving votes: <br/>
+          {props.publishedPolls[id].others.map((team, index) => {
+            return <span style={{ fontSize: '25px' }}>{team.team.location}: {team.points},{'  '}</span>
+          })}
+        </div>
+      </div>
+    )
+  } else{
+    return(
+      <div className="lds-dual-ring"></div>
+    )
   }
 }
 
