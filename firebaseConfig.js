a427b408eb92e166ee420a606be86862a4b2df9c
Mark Hayford <mhayford29@gmail.com>
2019-12-08 15:25:35 -0800

    removed file from gitignore

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+const FIREBASE_KEY = process.env.FIREBASE_KEY
+
+module.exports = {
+  apiKey: FIREBASE_KEY,
+  authDomain: "cfb-ranker.firebaseapp.com",
+  databaseURL: "https://cfb-ranker.firebaseio.com",
+  projectId: "cfb-ranker",
+  storageBucket: "cfb-ranker.appspot.com",
+  messagingSenderId: "653104447417",
+  appId: "1:653104447417:web:588cc3d422670d9f860068",
+  measurementId: "G-XC44H26XEP"
+};
