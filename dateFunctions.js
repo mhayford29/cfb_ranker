5e57f2c824531e5d2754d75712e1d99633ed1bde
Mark Hayford <mhayford29@gmail.com>
2020-02-21 13:15:01 -0800

    added isValidWeek method

--- dateFunctions.js
+++ dateFunctions.js
@@ -1,16 +1,22 @@
 var moment = require('moment');
 
 module.exports = {
-  getWeekNumber: () => {
-    var start = new Date('2019-08-24 23:00:00');
+  getWeekNumber: function() {
+    //start is the date of the first saturday of the season, not the first full saturday of the season
+    var start = new Date('2020-08-29 23:00:00');
     var end = new Date();
     
     var diff = new moment.duration(end.getTime() - start.getTime())
     
     return Math.floor(diff.asWeeks()) + 1;
   },
-  isValidDay: () => {
+  isValidDay: function() {
     var currentDate = new Date();
     return currentDate.getDay() < 2 ? true : false
+  },
+  isValidWeek: function() {
+    var weekNumber = this.getWeekNumber()
+    return weekNumber < 0 ? true : false
   }
 }
+
